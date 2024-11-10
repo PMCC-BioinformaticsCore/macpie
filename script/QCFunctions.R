5050f9d960b5364beecbf2627a5a16676e3f84e8
qoiopipq <xin.liu@petermac.org>
2024-11-11 10:22:48 +1100

    Update QCFunctions.R

--- script/QCFunctions.R
+++ script/QCFunctions.R
@@ -5,106 +5,118 @@ library(gridExtra)
 library(RColorBrewer)
 library(ggrepel)
 ####################### Seurat's clustering ######################
-clustering <- function(mac) {
-  sct <- mac %>%
-    PercentageFeatureSet(., pattern = "^MT-", col.name = "percent.mt") %>%
-    PercentageFeatureSet(., pattern = "^RP[SL][[:digit:]]|^RPLP[[:digit:]]|^RPSA",
-                         col.name = "percent.ribo") %>%
-    SCTransform(do.scale = T, return.only.var.genes = F, vars.to.regress = "percent.mt",
-                verbose = FALSE) %>%
-    FindVariableFeatures(verbose = FALSE) %>%
-    RunPCA(verbose = FALSE) %>%
-    FindNeighbors(verbose = FALSE) %>%
-    FindClusters(method = "igraph", verbose = FALSE)
-  return(sct)
+clustering <- function(mac, npcs=50) {
+  if (ncol(mac)>50){
+    sct <- mac %>%
+      SCTransform(do.scale = T, return.only.var.genes = F, vars.to.regress = "percent.mt",
+                  verbose = FALSE) %>%
+      FindVariableFeatures(verbose = FALSE) %>%
+      RunPCA(verbose = FALSE, npcs = npcs) %>%
+      FindNeighbors(verbose = FALSE) %>%
+      FindClusters(method = "igraph", verbose = FALSE)
+    return(sct)
+    
+  }else{
+    npcs=ncol(mac)-1
+    sct <- mac %>%
+      SCTransform(do.scale = T, return.only.var.genes = F, vars.to.regress = "percent.mt",
+                  verbose = FALSE) %>%
+      FindVariableFeatures(verbose = FALSE) %>%
+      RunPCA(verbose = FALSE, npcs = npcs) %>%
+      FindNeighbors(verbose = FALSE) %>%
+      FindClusters(method = "igraph", verbose = FALSE)
+    return(sct)
+    
+  }
+
 }
 
 plot_clusters <- function(mac) {
   p1 <- FeaturePlot(mac, features = "nCount_RNA")
   p2 <- FeaturePlot(mac, features = "percent.mt")
   p3 <- FeaturePlot(mac, features = "percent.ribo")
+  p4 <- DimPlot(mac, group.by = "Sample_type")
   p4 <- DimPlot(mac, group.by = "Row")
   p5 <- DimPlot(mac, group.by = "Column")
   p6 <- DimPlot(mac, group.by = "Concentration_1")
   p7 <- DimPlot(mac, group.by = "seurat_clusters")
   p8 <- DimPlot(mac, group.by = "Cell_type")
-  p10 <- DimPlot(mac, group.by = "Sample_type")
-  p9 <- DimPlot(mac, group.by = "edge_wells", split.by = "Cell_type")
-  plot(CombinePlots(plots = list(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10), nrow = 3))
+  p9 <- DimPlot(mac, group.by = "Treatment_1")
+  plot(CombinePlots(plots = list(p1, p2, p3, p4, p5, p6, p7, p8, p9), nrow = 3))
 }
 
 
 
 
 ################ plot QC #################
-### not using this. refer to plot_grid_platelayout function at the bottom ###
 
 #function for plotting QC
-# plot_QC<-function(data,feature_type,treatment){
-#   data<-data %>%
-#     select(Well_ID,Row,Column,{{feature_type}},{{treatment}}) %>% 
-#     mutate(Col=as.character(Column)) %>%
-#     mutate(Col=factor(Col,levels=mixedsort(unique(Col)))) %>%
-#     mutate(median_value=median(!!rlang::sym(feature_type))) %>%
-#     mutate(max_value=max(!!rlang::sym(feature_type))) %>%
-#     mutate(min_value=min(!!rlang::sym(feature_type)))
-#   p<-ggplot(data, aes(Col,
-#                       fct_rev(as_factor(Row)),
-#                       fill=!!rlang::sym(feature_type),
-#   )
-#   ) + 
-#     geom_tile() +
-#     scale_x_discrete(position = "top") +
-#     #scale_fill_continuous(trans="log10") +
-#     scale_fill_gradient2(low = "blue",
-#                          mid = "white",
-#                          high = "red",
-#                          midpoint = unique(data$median_value),
-#                          name = "RNA read counts") +
-#     theme(
-#       panel.grid = element_blank(),
-#       axis.title.y = element_blank(),
-#       axis.title.x = element_blank()
-#     ) +
-#     geom_text(aes(label=!!rlang::sym(treatment)),size=2)
-#   p
-# }
+plot_QC<-function(data,feature_type,treatment){
+  data<-data %>%
+    dplyr::select(Well_ID,Row,Column,{{feature_type}},{{treatment}}) %>% 
+    mutate(Col=as.character(Column)) %>%
+    mutate(Col=factor(Col,levels=mixedsort(unique(Col)))) %>%
+    mutate(median_value=median(!!rlang::sym(feature_type))) %>%
+    mutate(max_value=max(!!rlang::sym(feature_type))) %>%
+    mutate(min_value=min(!!rlang::sym(feature_type)))
+  p<-ggplot(data, aes(Col,
+                      fct_rev(as_factor(Row)),
+                      fill=!!rlang::sym(feature_type),
+  )
+  ) + 
+    geom_tile() +
+    scale_x_discrete(position = "top") +
+    #scale_fill_continuous(trans="log10") +
+    scale_fill_gradient2(low = "blue",
+                         mid = "white",
+                         high = "red",
+                         midpoint = unique(data$median_value),
+                         name = "RNA read counts") +
+    theme(
+      panel.grid = element_blank(),
+      axis.title.y = element_blank(),
+      axis.title.x = element_blank()
+    ) +
+    geom_text(aes(label=!!rlang::sym(treatment)),size=2)
+  p
+}
 
 #function for plotting QC with changing legend title 
-# plot_QC_name<-function(data,feature_type,treatment, attribute_name){
-#   data<-data %>%
-#     select(Well_ID,Row,Column,{{feature_type}},{{treatment}}) %>% 
-#     mutate(Col=as.character(Column)) %>%
-#     mutate(Col=factor(Col,levels=mixedsort(unique(Col)))) %>%
-#     mutate(median_value=median(!!rlang::sym(feature_type))) %>%
-#     mutate(max_value=max(!!rlang::sym(feature_type))) %>%
-#     mutate(min_value=min(!!rlang::sym(feature_type)))
-#   p<-ggplot(data, aes(Col,
-#                       fct_rev(as_factor(Row)),
-#                       fill=!!rlang::sym(feature_type),
-#   )
-#   ) + 
-#     geom_tile() +
-#     scale_x_discrete(position = "top") +
-#     #scale_fill_continuous(trans="log10") +
-#     scale_fill_gradient2(low = "blue",
-#                          mid = "white",
-#                          high = "red",
-#                          midpoint = unique(data$median_value),
-#                          name = paste0(attribute_name)) +
-#     theme(
-#       panel.grid = element_blank(),
-#       axis.title.y = element_blank(),
-#       axis.title.x = element_blank()
-#     ) +
-#     geom_text(aes(label=!!rlang::sym(treatment)),size=2)
-#   p
-# }
+plot_QC_name<-function(data,feature_type,treatment, attribute_name){
+  data<-data %>%
+    dplyr::select(Well_ID,Row,Column,{{feature_type}},{{treatment}}) %>% 
+    mutate(Col=as.character(Column)) %>%
+    mutate(Col=factor(Col,levels=mixedsort(unique(Col)))) %>%
+    mutate(median_value=median(!!rlang::sym(feature_type))) %>%
+    mutate(max_value=max(!!rlang::sym(feature_type))) %>%
+    mutate(min_value=min(!!rlang::sym(feature_type)))
+  p<-ggplot(data, aes(Col,
+                      fct_rev(as_factor(Row)),
+                      fill=!!rlang::sym(feature_type),
+  )
+  ) + 
+    geom_tile() +
+    scale_x_discrete(position = "top") +
+    #scale_fill_continuous(trans="log10") +
+    scale_fill_gradient2(low = "blue",
+                         mid = "white",
+                         high = "red",
+                         midpoint = unique(data$median_value),
+                         name = paste0(attribute_name)) +
+    theme(
+      panel.grid = element_blank(),
+      axis.title.y = element_blank(),
+      axis.title.x = element_blank()
+    ) +
+    geom_text(aes(label=!!rlang::sym(treatment)),size=2)
+  p
+}
 
 
 
 ################ plot RLE #################
 RLEplot<-function(count_matrix,ID,feature){
+  count_matrix<-log2(count_matrix+1)
   count_matrix<-as.matrix(count_matrix)
   colnames(count_matrix)<-as.factor(ID)
   rle<-count_matrix-Biobase::rowMedians(count_matrix)
@@ -129,13 +141,13 @@ RLEplot<-function(count_matrix,ID,feature){
 ################platetools QC plate layout#################
 ################log scale option#################
 plot_grid_platelayout<-function(data, feature_type, legend_label, title_label,n_wells=384, log_scale=FALSE){
-  data<-data%>%select(Well_ID, {{feature_type}})
+  data<-data%>%dplyr::select(Well_ID, {{feature_type}})
   if (log_scale==TRUE){
     data[{{feature_type}}]<-log10(data[{{feature_type}}])
     min_value<-min(data[{{feature_type}}][,1])
     median_value<-median(data[{{feature_type}}][,1])
     max_value<-max(data[{{feature_type}}][,1])
-    p<-raw_map(data=data[{{feature_type}}][,1], well=data$Well_ID, plate=n_wells)+ggtitle(paste0(params$project_code, ": ", title_label)) +
+    p<-raw_map(data=data[{{feature_type}}][,1], well=data$Well_ID, plate=n_wells)+ggtitle(paste0(params$plate_code, ": ", title_label)) +
       scale_fill_gradient2(
         low = "blue",
         mid = "white",
