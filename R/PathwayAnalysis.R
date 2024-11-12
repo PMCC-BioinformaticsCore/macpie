18f1074fad26afab4f8bb9b97bb51411015f1add
Xin Liu <xin.liu@petermac.org>
2024-11-12 16:45:51 +1100

    Add documentation for pathway functions

--- R/PathwayAnalysis.R
+++ R/PathwayAnalysis.R
@@ -1,15 +1,13 @@
 
-suppressMessages(library(dplyr))
-suppressMessages(library(httr2))
-suppressMessages(library(ggplot2))
-suppressMessages(library(DT))
-
-### writing functions ###
-#get database from enrichR
-
-#' 1. connect to enrichR
+#' Connect to enrichR
+#' This function connect to enrichR database
 #' here, we only focus on main enrichR, not fish/fly/yeastenrichR
 #' enrichR contains both human and mouse
+#' @param dbs name of database in encirhR such as main enrichR or fish/fly/yeastenrichR
+#' @return link for enrichR database
+#' @export
+
+#' @import dyplyr htt2 ggplot2 DT
 enrichr_url<-function(dbs="enrichr"){
   dbs<-match.arg(dbs)
   switch(dbs,
@@ -20,8 +18,12 @@ enrichr_url<-function(dbs="enrichr"){
 
 
 #' 2.check enrichr connection and get the stats of datasets from enrichR
+#' This function returns a data frame with all available data sets from enrichR
 #' from enrichR script, the listEnrichrSites function extracts the big table of all available datasets
 #' from "datasetStatistics"
+#' @param url link of the enrichR database
+#' @return data frame of all available data sets from the enrichR link user provided
+#' @export
 GS_summary<-function(url){
   resp<-request(paste0(url,"datasetStatistics"))|> req_perform()
   if(!resp_status_desc(resp)=="OK"){
@@ -44,7 +46,11 @@ GS_summary<-function(url){
 
 
 
-#' 3. download those datasets
+#' Download dataset of interest
+#' @param url link of enrichR database
+#' @param geneset name of data sets from the enrichR database such as KEGG
+#' @export
+#' @return list of pathways contains sub-lists of genes that annotated in the corresponding pathways
 geneset_download<-function(url, geneset){
   resp_lib<-request(paste0(url,"geneSetLibrary?mode=text&libraryName=",geneset))|> req_perform()
   resp_lib_split<-strsplit(resp_body_string(resp_lib), split = "\n")[[1]]
@@ -62,8 +68,15 @@ geneset_download<-function(url, geneset){
 
 
 
-#' 4. Gene enrichment analysis
-#' make it as a function for hyper-geometric test
+#' Perform gene enrichment analysis
+#' by doing a hyper-geometric test
+#' @param deg list of DEGs
+#' @param genesets selected pathway datasets like KEGG from enrichR
+#' @param background number of genes that considered as background, options as "human": number of human genes
+#' or "geneset" genes in the previously selected pathway dataset
+#' @export
+#' @return a data frame of enrichment analysis results with all the statistics
+
 hyper_enrich_bg<-function(deg, #list of DEGs
                           genesets, #pathway from enrichr
                           background="human" #define the number of genes in your experiment (interger), or "human" to use human genome or
@@ -123,7 +136,11 @@ hyper_enrich_bg<-function(deg, #list of DEGs
 
 }
 
-#' 5. basic bar plot
+
+#' generate a basic bar plot
+#' @param df output data frame from enrichment analysis
+#' @export
+#' @return bar plot showing top 10 enriched pathways
 plot_enrich<-function(df, numTerms=10){
   map<-as.numeric(sub("/.*","", df$Overlap))
   gsn<-as.numeric(sub(".*/","", df$Overlap))
