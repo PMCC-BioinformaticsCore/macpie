3758164944c0d2f750b005d07d950b94715cb5c4
nbartonicek <nbartonicek@gmail.com>
2024-12-03 11:55:25 +1100

    Clean main

---
+++ scripts/1.full_analysis.R
@@ -0,0 +1,98 @@
+library(devtools)
+library(tidyverse)
+library(Seurat)
+library(gtools)
+library(tidyseurat)
+library(DESeq2)
+library(edgeR)
+library(RUVSeq)
+library(variancePartition)
+library(reshape2)
+library(gridExtra)
+library(ggrepel)
+library(lintr)
+
+#define longer length for description files
+custom_linters <- lintr::linters_with_defaults(
+  line_length_linter = lintr::line_length_linter(120) # Set max line length to 120
+)
+
+#load all previous libraries
+load_all()
+
+#select which sample dataset to use
+project_name<-"PMMSq033"
+
+#directory with data
+data_dir<-"inst/extdata/"
+project_metadata<-paste0(data_dir,project_name,"/",project_name,"_metadata.csv")
+project_rawdata<-paste0(data_dir,project_name,"/raw_matrix")
+
+#load metadata
+######## 1. Mark's function
+#metadata<-read_metadata()
+#TO-DO: fix the function so that it doesn't open a window and can be used from the command line
+#TO-DO: check for importing cell count & FACS data
+
+metadata<-read.csv(project_metadata,header=T) %>%
+  arrange("Barcode") %>%
+  mutate(Treatment_1=gsub("-","_",Treatment_1)) %>%
+  mutate(Treatment_conc=paste0(Treatment_1,"_",Concentration_1))
+
+#only create an id if there are multiple plates
+#TO-DO: test a project with multiple plates
+if(length(project_metadata)>1){
+  metadata<-metadata %>%
+    mutate(id=gsub("Plate","",Plate_ID)) %>%
+    mutate(Barcode=paste0(id,"_",Barcode))
+}
+
+######## 2. Mark's function
+#validate_metadata(metadata)
+
+#import reads
+# TO-DO: check for multiple folders of data that should be imported at the same time
+raw_counts_total <- Read10X(data.dir = project_rawdata)
+keep <- rowSums(cpm(raw_counts_total)>=10) >= 2
+raw_counts <- raw_counts_total[keep,]
+
+#create tidySeurat object
+mac <- CreateSeuratObject(counts = raw_counts,
+                          project = project_name,
+                          min.cells = 1,
+                          min.features = 1)
+
+#calculate percent of mitochondrial and ribosomal genes
+mac[["percent.mt"]] <- PercentageFeatureSet(mac, pattern = "^mt-|^MT-")
+mac[["percent.ribo"]] <- PercentageFeatureSet(mac, pattern = "^Rp[sl][[:digit:]]|^Rplp[[:digit:]]|^Rpsa|^Rp[sl][[:digit:]]|^Rplp[[:digit:]]|^Rpsa|
+                                                ^RP[SL][[:digit:]]|^RPLP[[:digit:]]|^RPSA|^RP[SL][[:digit:]]|^RPLP[[:digit:]]|^RPSA")
+#TO-DO: verify that the mouse and human regexp for ribo proteins is correct (grepl("^RP",row.names(mac@assays$RNA$counts)))
+
+#add metadata to the real data
+mac<- mac %>%
+  inner_join(metadata,by=c(".cell"="Barcode"))
+
+#QC plots
+VlnPlot(mac, features = c("nFeature_RNA", "nCount_RNA", "percent.mt","percent.ribo"), ncol = 4)
+#TO-DO: verify that different plates would be plotted side-by-side
+
+
+############ PROCEDURE TO MAKE A FUNCTION
+##1. create a function
+#use_r(functionX)
+## edit script
+#load_all()
+##check if the function exists in the global environment
+#exists("plot_plate_layout", where = globalenv(), inherits = FALSE)
+#check()
+#document()
+#check()
+#lint(filename="R/functionX.R",linters = custom_linters)
+#use_test("functionX")
+
+
+
+
+
+
+
---
+++ scripts/QCFunctions.R
@@ -0,0 +1,186 @@
+suppressMessages(library(ggplot2))
+library(SeuratObject)
+library(gridExtra)
+
+library(RColorBrewer)
+library(ggrepel)
+####################### Seurat's clustering ######################
+clustering <- function(mac, npcs=50) {
+  if (ncol(mac)>50){
+    sct <- mac %>%
+      SCTransform(do.scale = T, return.only.var.genes = F, vars.to.regress = "percent.mt",
+                  verbose = FALSE) %>%
+      FindVariableFeatures(verbose = FALSE) %>%
+      RunPCA(verbose = FALSE, npcs = npcs) %>%
+      FindNeighbors(verbose = FALSE) %>%
+      FindClusters(method = "igraph", verbose = FALSE)
+    return(sct)
+    
+  }else{
+    npcs=ncol(mac)-1
+    sct <- mac %>%
+      SCTransform(do.scale = T, return.only.var.genes = F, vars.to.regress = "percent.mt",
+                  verbose = FALSE) %>%
+      FindVariableFeatures(verbose = FALSE) %>%
+      RunPCA(verbose = FALSE, npcs = npcs) %>%
+      FindNeighbors(verbose = FALSE) %>%
+      FindClusters(method = "igraph", verbose = FALSE)
+    return(sct)
+    
+  }
+
+}
+
+plot_clusters <- function(mac) {
+  p1 <- FeaturePlot(mac, features = "nCount_RNA")
+  p2 <- FeaturePlot(mac, features = "percent.mt")
+  p3 <- FeaturePlot(mac, features = "percent.ribo")
+  p4 <- DimPlot(mac, group.by = "Sample_type")
+  p4 <- DimPlot(mac, group.by = "Row")
+  p5 <- DimPlot(mac, group.by = "Column")
+  p6 <- DimPlot(mac, group.by = "Concentration_1")
+  p7 <- DimPlot(mac, group.by = "seurat_clusters")
+  p8 <- DimPlot(mac, group.by = "Cell_type")
+  p9 <- DimPlot(mac, group.by = "Treatment_1")
+  plot(CombinePlots(plots = list(p1, p2, p3, p4, p5, p6, p7, p8, p9), nrow = 3))
+}
+
+
+
+
+################ plot QC #################
+
+#function for plotting QC
+plot_QC<-function(data,feature_type,treatment){
+  data<-data %>%
+    dplyr::select(Well_ID,Row,Column,{{feature_type}},{{treatment}}) %>% 
+    mutate(Col=as.character(Column)) %>%
+    mutate(Col=factor(Col,levels=mixedsort(unique(Col)))) %>%
+    mutate(median_value=median(!!rlang::sym(feature_type))) %>%
+    mutate(max_value=max(!!rlang::sym(feature_type))) %>%
+    mutate(min_value=min(!!rlang::sym(feature_type)))
+  p<-ggplot(data, aes(Col,
+                      fct_rev(as_factor(Row)),
+                      fill=!!rlang::sym(feature_type),
+  )
+  ) + 
+    geom_tile() +
+    scale_x_discrete(position = "top") +
+    #scale_fill_continuous(trans="log10") +
+    scale_fill_gradient2(low = "blue",
+                         mid = "white",
+                         high = "red",
+                         midpoint = unique(data$median_value),
+                         name = "RNA read counts") +
+    theme(
+      panel.grid = element_blank(),
+      axis.title.y = element_blank(),
+      axis.title.x = element_blank()
+    ) +
+    geom_text(aes(label=!!rlang::sym(treatment)),size=2)
+  p
+}
+
+#function for plotting QC with changing legend title 
+plot_QC_name<-function(data,feature_type,treatment, attribute_name){
+  data<-data %>%
+    dplyr::select(Well_ID,Row,Column,{{feature_type}},{{treatment}}) %>% 
+    mutate(Col=as.character(Column)) %>%
+    mutate(Col=factor(Col,levels=mixedsort(unique(Col)))) %>%
+    mutate(median_value=median(!!rlang::sym(feature_type))) %>%
+    mutate(max_value=max(!!rlang::sym(feature_type))) %>%
+    mutate(min_value=min(!!rlang::sym(feature_type)))
+  p<-ggplot(data, aes(Col,
+                      fct_rev(as_factor(Row)),
+                      fill=!!rlang::sym(feature_type),
+  )
+  ) + 
+    geom_tile() +
+    scale_x_discrete(position = "top") +
+    #scale_fill_continuous(trans="log10") +
+    scale_fill_gradient2(low = "blue",
+                         mid = "white",
+                         high = "red",
+                         midpoint = unique(data$median_value),
+                         name = paste0(attribute_name)) +
+    theme(
+      panel.grid = element_blank(),
+      axis.title.y = element_blank(),
+      axis.title.x = element_blank()
+    ) +
+    geom_text(aes(label=!!rlang::sym(treatment)),size=2)
+  p
+}
+
+
+
+################ plot RLE #################
+RLEplot<-function(count_matrix,ID,feature){
+  count_matrix<-log2(count_matrix+1)
+  count_matrix<-as.matrix(count_matrix)
+  colnames(count_matrix)<-as.factor(ID)
+  rle<-count_matrix-Biobase::rowMedians(count_matrix)
+  feature<-as.factor(feature)
+  rle<-rle[, sort.list(feature)]
+  rledf <- t(apply(rle, 2, function(x) grDevices::boxplot.stats(x)$stats))
+  rledf <- as.data.frame(rledf)
+  colnames(rledf) <- c("ymin", "lower", "middle", "upper", "ymax")
+  rledf$feature<-as.factor(feature[sort.list(feature)])
+  rledf$sample<-rownames(rledf)
+  rledf$sample<-reorder(as.factor(rledf$sample), unique(rledf$sample))#updated
+  
+  p<-ggplot(rledf, aes(x=sample, fill=feature))+geom_boxplot(aes(ymin=ymin, lower=lower, middle=middle, upper=upper, ymax=ymax), stat="identity")+theme_bw()+theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+scale_x_discrete(limits=rledf$sample)+
+    scale_color_manual(values = colorRampPalette(brewer.pal(12, "Paired"))(nlevels(as.factor(feature))))+
+    geom_hline(yintercept = 0, linetype="dotted", col="red", size=1)+ylab("RLE")
+  p
+}
+
+
+
+
+################platetools QC plate layout#################
+################log scale option#################
+plot_grid_platelayout<-function(data, feature_type, legend_label, title_label,n_wells=384, log_scale=FALSE){
+  data<-data%>%dplyr::select(Well_ID, {{feature_type}})
+  if (log_scale==TRUE){
+    data[{{feature_type}}]<-log10(data[{{feature_type}}])
+    min_value<-min(data[{{feature_type}}][,1])
+    median_value<-median(data[{{feature_type}}][,1])
+    max_value<-max(data[{{feature_type}}][,1])
+    p<-raw_map(data=data[{{feature_type}}][,1], well=data$Well_ID, plate=n_wells)+ggtitle(paste0(params$plate_code, ": ", title_label)) +
+      scale_fill_gradient2(
+        low = "blue",
+        mid = "white",
+        high = "red",
+        midpoint = median_value,
+        limits = c(floor(min_value), ceiling(max_value)),
+        name = paste0(legend_label)
+      )+theme(
+        plot.title = element_text(size = 15),
+        axis.text.x = element_text(size = 8),
+        axis.text.y = element_text(size = 8)
+      )
+    p
+    
+  }else{
+    min_value<-min(data[{{feature_type}}][,1])
+    median_value<-median(data[{{feature_type}}][,1])
+    max_value<-max(data[{{feature_type}}][,1])
+    p<-raw_map(data=data[{{feature_type}}][,1], well=data$Well_ID, plate=n_wells)+ggtitle(paste0(params$project_code, ": ", title_label)) +
+      scale_fill_gradient2(
+        low = "blue",
+        mid = "white",
+        high = "red",
+        midpoint = median_value,
+        limits = c(floor(min_value), ceiling(max_value)),
+        name = paste0(legend_label)
+      )+  theme(
+        plot.title = element_text(size = 15),
+        axis.text.x = element_text(size = 8),
+        axis.text.y = element_text(size = 8)
+      )
+    p
+  }
+}
+
+
