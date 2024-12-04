0919fe0c8507c998db7f898038735fd678f2d2fe
susanneramm1 <susanne.ramm@gmail.com>
2024-12-04 12:38:19 +0000

    adding first version

--- R/metadata_heatmap.R
+++ R/metadata_heatmap.R
@@ -1,3 +1,5 @@
+utils::globalVariables(c(".data", "Plate_ID"))
+
 #' Generate Heatmaps of Metadata Function
 #'
 #' This function generates heatmaps from metadata, either from an existing
@@ -10,44 +12,51 @@
 #' @return Displays the plot as a ggplot object or saves it as a file (jpg, png, or pdf).
 #' @export
 #'
-#' @examples
-#' generate_heatmaps("~/Documents/metadata_test.csv")
-#' generate_heatmaps("~/Documents/metadata_test.csv", legend = "none")
-#' generate_heatmaps("~/Documents/metadata_test.csv",output_file = "~/Documents/metadata_graph.png")
+#' @importFrom readr read_csv
+#' @importFrom dplyr %>% mutate filter
+#' @importFrom stringr str_extract
+#' @importFrom ggplot2 ggplot aes geom_tile labs coord_fixed theme_minimal theme element_text element_rect scale_fill_viridis_c scale_fill_viridis_d
+#' @importFrom patchwork wrap_plots plot_annotation
+#' @importFrom grid unit
+#' @importFrom ggplot2 ggsave
 #'
-generate_heatmaps <- function(metadata_file = NULL, legend = "show", output_file = NULL) {
-  # Check if 'cleaned_metadata' exists in the global environment
+#' @examples
+#' temp_file <- tempfile(fileext = ".csv")
+#' write.csv(
+#'   data.frame(Well_ID = c("A01", "A02", "B01", "B02"),
+#'   Plate_ID = c("P1", "P1","P1", "P1"),
+#'   Compound_ID = c("C1", "C2", "C3", "C4")),
+#'   temp_file, row.names = FALSE
+#' )
+#' metadata_heatmap(temp_file)
+
+
+metadata_heatmap <- function(metadata_file = NULL, legend = "show", output_file = NULL) {
   if (exists("cleaned_metadata", where = .GlobalEnv)) {
     message("Using existing 'cleaned_metadata' from the global environment.")
     metadata <- get("cleaned_metadata", envir = .GlobalEnv)
   } else {
-    # If not, check if a file was provided
     if (is.null(metadata_file)) {
       stop("No 'cleaned_metadata' found and no file provided. Please provide a CSV file.")
     }
-    # Read the metadata CSV
     metadata <- read_csv(metadata_file, show_col_types = FALSE)
   }
 
-  # Validate the Well_ID column
   if (!"Well_ID" %in% colnames(metadata)) {
     stop("The metadata is missing a 'Well_ID' column.")
   }
 
-  # Check for valid Well_ID format
   valid_well_id <- grepl("^[A-P](0[1-9]|1[0-9]|2[0-4])$", metadata$Well_ID)
   if (!all(valid_well_id)) {
-    stop("The 'Well_ID' column contains invalid entries. It must consist of a letter A-P followed by a number between 01 and 24.")
+    stop("The 'Well_ID' column contains invalid entries.")
   }
 
-  # Extract row and column numbers from Well_ID
   metadata <- metadata %>%
     mutate(
-      row = as.numeric(factor(str_extract(.data$Well_ID, "^[A-P]"))),
-      col = as.numeric(str_extract(.data$Well_ID, "\\d+"))
+      row = as.numeric(factor(str_extract(metadata$Well_ID, "^[A-P]"))),
+      col = as.numeric(str_extract(metadata$Well_ID, "\\d+"))
     )
 
-  # Validate the Plate_ID column
   if (!"Plate_ID" %in% colnames(metadata)) {
     stop("The metadata is missing a 'Plate_ID' column.")
   }
@@ -55,32 +64,29 @@ generate_heatmaps <- function(metadata_file = NULL, legend = "show", output_file
   # List of all annotation columns excluding 'Well_ID', 'row', 'Row', 'Column', and 'col'
   annotation_cols <- setdiff(names(metadata), c("Well_ID", "row", "col", "Row", "Column", "Plate_ID"))
 
-  # Check for columns with more than 30 unique values
+  # Check if annotation_cols is empty
+  if (length(annotation_cols) == 0) {
+    stop("No annotation columns found to generate heatmaps. Please check your input data.")
+  }
+
   hide_legends <- sapply(annotation_cols, function(col) {
     length(unique(metadata[[col]])) > 20
   })
 
-  # Iterate over unique Plate_IDs
   unique_plate_ids <- unique(metadata$Plate_ID)
   for (plate_id in unique_plate_ids) {
-    # Filter data for the current Plate_ID
     plate_data <- metadata %>% filter(Plate_ID == plate_id)
 
-    # Generate heatmaps for each annotation column
     heatmaps <- lapply(seq_along(annotation_cols), function(i) {
       annotation <- annotation_cols[i]
-
-      # Determine scale type based on annotation data
-      if (is.numeric(plate_data[[annotation]])) {
-        scale_func <- scale_fill_viridis_c(option = "C", na.value = "grey80")
+      scale_func <- if (is.numeric(plate_data[[annotation]])) {
+        scale_fill_viridis_c(option = "C", na.value = "grey80")
       } else {
-        scale_func <- scale_fill_viridis_d(option = "C", na.value = "grey80")
+        scale_fill_viridis_d(option = "C", na.value = "grey80")
       }
 
-      # Determine if legend should be hidden
       legend_setting <- if (legend == "show" && !hide_legends[i]) "right" else "none"
 
-      # Create the heatmap
       ggplot(plate_data, aes(x = col, y = row, fill = .data[[annotation]])) +
         geom_tile(color = "white") +
         scale_func +
@@ -102,7 +108,11 @@ generate_heatmaps <- function(metadata_file = NULL, legend = "show", output_file
         )
     })
 
-    # Combine heatmaps for this Plate_ID using patchwork
+    # Check if heatmaps is empty
+    if (length(heatmaps) == 0) {
+      stop("No heatmaps were generated. Please check the input metadata and annotation columns.")
+    }
+
     combined_plot <- wrap_plots(heatmaps, ncol = 3) +
       plot_annotation(
         title = paste("macpie Metadata Heatmap Grid - Plate ID:", plate_id),
@@ -111,7 +121,6 @@ generate_heatmaps <- function(metadata_file = NULL, legend = "show", output_file
         )
       )
 
-    # Save or display the plot
     if (!is.null(output_file)) {
       file_name <- paste0(sub(".(png|jpg|pdf)$", "", output_file), "_Plate_", plate_id, ".", tools::file_ext(output_file))
       ggsave(file_name, combined_plot, width = 10, height = 6 * ceiling(length(heatmaps) / 3), units = "in")
