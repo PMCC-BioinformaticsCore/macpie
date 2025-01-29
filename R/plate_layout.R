37e81988435102da606e96bbb29901cbac8e6b08
nbartonicek <nbartonicek@gmail.com>
2025-01-29 12:31:14 +1100

    Fix colors

--- R/plate_layout.R
+++ R/plate_layout.R
@@ -68,9 +68,9 @@ plate_layout <- function(data = NULL, metric = NULL, annotation = NULL) {
                           fill = !!rlang::sym(metric))) +
       geom_tile() +
       scale_x_discrete(position = "top") +
-      scale_fill_gradient2(low = "#3C5488FF",
+      scale_fill_gradient2(high = "#E64B35FF",
                            mid = "white",
-                           high = "#E64B35FF",
+                           low = "#3C5488FF",
                            midpoint = unique(data$median_value),
                            name = {{metric}}) +
       theme(
