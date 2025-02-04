b40bbbf1e54958e7d5b9d470442bf5fc24b88a23
nbartonicek <nbartonicek@gmail.com>
2025-02-04 13:41:58 +1100

    Add multi_enrich_pathways

--- tests/testthat/test-multi_enrich_pathways.R
+++ tests/testthat/test-multi_enrich_pathways.R
@@ -1,12 +1,8 @@
-test_that("Results are a non-empty list", {
+test_that("Results are a non-empty dataframe in seurat slot tools", {
   file_path <- system.file("extdata", "PMMSq033/PMMSq033.rds", package = "macpie")
-  mac <- readRDS(file_path)[50:150]
-  control_samples<-"DMSO_0"
-  treatment_samples <- mac %>%
-    select(combined_id) %>%
-    filter(!grepl("DMSO", combined_id)) %>%
-    pull() %>%
-    unique()
-  mac <- multi_DE(mac, treatment_samples, control_samples, num_cores = 2, method = "edgeR")
-  expect_true(length(mac) > 0 && ncol(mac@tools[["diff_exprs"]][[1]]) == 6)
+  mac <- readRDS(file_path)
+  file_path <- system.file("extdata", "PMMSq033/pathways.Rds", package = "macpie")
+  genesets <- readRDS(file_path)
+  mac <- multi_enrich_pathways(mac, genesets = genesets)
+  expect_true(length(mac) > 0 && nrow(mac@tools[["pathway_enrichment"]][[1]]) > 0)
 })
