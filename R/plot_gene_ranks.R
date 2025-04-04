1a9b072021a6b72dc1c295b6620e31f7770a49eb
Xin Liu <xin.liu@petermac.org>
2025-04-04 14:45:41 +1100

    add log option in plot_gene_ranks

--- R/plot_gene_ranks.R
+++ R/plot_gene_ranks.R
@@ -11,14 +11,15 @@
 #' @examples
 #' file_path <- system.file("extdata", "PMMSq033/PMMSq033.rds", package = "macpie")
 #' mac <- readRDS(file_path)
-#' plot_rank_counts(mac, group_by = "combined_id", samples = "Staurosporine_10")
+#' plot_gene_ranks(mac, group_by = "combined_id", samples = "Staurosporine_10", scale_y_log = TRUE)
 
 
 plot_gene_ranks <- function(data = NULL,
                             group_by = NULL,
-                            samples = NULL) {
+                            samples = NULL,
+                            scale_y_log = TRUE) {
   # Helper function to validate input data
-  validate_inputs <- function(data, group_by, samples) {
+  validate_inputs <- function(data, group_by, samples, scale_y_log) {
     if (!inherits(data, "Seurat")) {
       stop("Error: argument 'data' must be a Seurat or TidySeurat object.")
     }
@@ -26,8 +27,11 @@ plot_gene_ranks <- function(data = NULL,
     if (is.null(samples)) {
       stop("Missing the vectors of samples.")
     }
+    if (!is.logical(scale_y_log) || length(scale_y_log) != 1) {
+      stop("Error: 'scale_y_log' must be a single logical value (TRUE or FALSE).")
+    }
   }
-  validate_inputs(data, group_by, samples)
+  validate_inputs(data, group_by, samples, scale_y_log)
   count_table <- as.data.frame(data@assays$RNA$count)
   combined_id_barcodes <- data@meta.data[[group_by]]
   names(combined_id_barcodes) <- rownames(data@meta.data)
@@ -48,8 +52,14 @@ plot_gene_ranks <- function(data = NULL,
       arrange(-sum)
     rank_counts_genes$rank <- seq_len(nrow(rank_counts_genes))
   }
-  p <- ggplot(rank_counts_genes, aes(x = rank, y = log10(sum))) +
-    geom_point(shape = 1) +
-    macpie_theme()
+  if (scale_y_log){
+    p <- ggplot(rank_counts_genes, aes(x = rank, y = log1p(sum))) +
+      geom_point(shape = 1) +
+      macpie_theme(show_x_title = TRUE, show_y_title = TRUE) 
+  } else {
+    p <- ggplot(rank_counts_genes, aes(x = rank, y = sum)) +
+      geom_point(shape = 1) +
+      macpie_theme(show_x_title = TRUE, show_y_title = TRUE)
+  }
   return(p)
 }
