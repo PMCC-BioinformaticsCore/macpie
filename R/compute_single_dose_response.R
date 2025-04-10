6c1a0b946c3cf5c1ba880b1d31adf7f02edeec45
nbartonicek <nbartonicek@gmail.com>
2025-04-10 10:25:06 +1000

    Single dose response update

--- R/compute_single_dose_response.R
+++ R/compute_single_dose_response.R
@@ -11,12 +11,12 @@
 #' @examples
 #' rds_file<-system.file("/extdata/PMMSq033/PMMSq033.rds", package = "macpie")
 #' mac<-readRDS(rds_file)
-#' res <- compute_single_dose_response(data = mac, gene = "SOX12", normalisation = "cpm", treatment_value = "Camptothecin")
+#' res <- compute_single_dose_response(data = mac, gene = "SOX12", normalisation = "limma_voom", treatment_value = "Staurosporine")
 #' res$plot
 #' }
 #' 
 #' @export
-compute_single_dose_response <- function(data, gene, normalisation = "cpm", treatment_value, batch=1) {
+compute_single_dose_response <- function(data, gene, normalisation = "limma_voom", treatment_value, batch=1) {
   
   # Sanity checks
   if (!gene %in% rownames(data)) stop("Gene not found in expression matrix.")
@@ -71,11 +71,37 @@ compute_single_dose_response <- function(data, gene, normalisation = "cpm", trea
   if (!is.null(model)) {
     newdata <- data.frame(concentration = seq(min(df$concentration), max(df$concentration), length.out = 100))
     newdata$predicted <- predict(model, newdata)
-    
+    ed <- ED(res$model, 50, interval = "delta")
+    ec50 <- ed[1, "Estimate"]
+    ec50_lower <- ed[1, "Lower"]
+    ec50_upper <- ed[1, "Upper"]
     p <- ggplot(df, aes(x = concentration, y = expression)) +
       geom_point(size = 2) +
       geom_line(data = newdata, aes(x = concentration, y = predicted), color = "blue", size = 1) +
-      scale_x_continuous(trans = scales::pseudo_log_trans(base = 10)) +
+      scale_x_continuous(
+        trans = pseudo_log_trans(base = 10),
+        breaks = c(0, 0.1, 1, 10, 100,1000),
+        labels = function(x) {
+          sapply(x, function(val) {
+            if (is.na(val)) {
+              NA  # skip or return blank if NA
+            } else if (abs(val - 0.1) < 1e-6) {
+              "0.1"
+            } else {
+              as.character(round(val))
+            }
+          })
+        }
+      ) +
+      geom_vline(xintercept = ec50, linetype = "dashed", color = macpie_colours$high) +
+      annotate("text",
+               x = ec50,
+               y = max(df$expression, na.rm = TRUE) * 0.95,  # 95% of the way up
+               label = sprintf("EC50 = %.2f\n[%.2f – %.2f]", ec50, ec50_lower, ec50_upper),
+               color = "black",
+               size = 4,
+               hjust = 0.5,
+               vjust = 1) +
       theme_minimal() +
       labs(
         title = paste("Sigmoidal fit for", gene, "with", treatment_value),
@@ -84,7 +110,7 @@ compute_single_dose_response <- function(data, gene, normalisation = "cpm", trea
       )
   } else {
     p <- ggplot(df, aes(x = concentration, y = expression)) +
-      geom_point(size = 2, color = "red") +
+      geom_point(size = 2, color = macpie_colours$high) +
       theme_minimal() +
       labs(title = "Model failed to fit", x = "Concentration", y = "Expression")
   }
