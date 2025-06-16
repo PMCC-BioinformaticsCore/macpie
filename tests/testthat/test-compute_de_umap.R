0e8fa513d6dea6da4992c30abdefca715e03bd51
nbartonicek <nbartonicek@gmail.com>
2025-06-16 17:10:52 +1000

    Fix test compute_de_umap

--- tests/testthat/test-compute_de_umap.R
+++ tests/testthat/test-compute_de_umap.R
@@ -1,5 +1,5 @@
 test_that("Results are a non-empty dataframe in seurat slot tools", {
   data("mini_mac")
   mini_mac <- compute_de_umap(mini_mac)
-  expect_true(length(mini_mac) > 0 && nrow(mini_mac@tools[["umap_de"]]) > 0)
+  expect_true(length(mini_mac) > 0 && nrow(mini_mac@reductions[["umap_de"]]) > 0)
 })
