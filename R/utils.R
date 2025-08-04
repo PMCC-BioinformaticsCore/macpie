994377eec628ea2abf998d06d1cf5b7b1ec4ec38
nbartonicek <nbartonicek@gmail.com>
2025-08-04 12:29:28 +1000

    Remove sys.env command

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,3 @@
 utils::globalVariables(c("diff_expressed", "gene_labels", ".cell",
                          "UMAPde_1", "UMAPde_2",
                          "padj", "target", "target_id"))
-Sys.setenv(MallocStackLogging = "0")
