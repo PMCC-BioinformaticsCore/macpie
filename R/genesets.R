2476957314eef675003c4a3a14cc712343006e4b
nbartonicek <nbartonicek@gmail.com>
2025-08-06 16:05:28 +1000

    Fix genesets

--- R/genesets.R
+++ R/genesets.R
@@ -3,11 +3,12 @@
 #' A named list of gene sets used for hypergeometric enrichment tests
 #' in the PMMSq033 dataset.
 #'
-#' @format A named list of length \code{length(genesets)}. Each element is
+#' @format A named list of length character vectors. Each element is
 #'   a character vector of gene symbols corresponding to one pathway.
 #'
 #' @source
 #' Generated from
 #' \code{extdata/PMMSq033/pathways.Rds}
 #' via the \code{data-raw/data.R} preprocessing script.
-"genesets"
+#' @name genesets
+NULL
