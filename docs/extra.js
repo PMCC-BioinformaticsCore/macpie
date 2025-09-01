cecf0c907739d2a726c31d935af433fce2e97eef
Xin Liu <xin.liu@petermac.org>
2025-09-01 16:31:55 +1000

    remove docs/ and pkgdown/

--- docs/extra.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const toc = document.getElementById("toc");
-  if (!toc) return;
-  toc.querySelector("h2").addEventListener("click", () => {
-    toc.classList.toggle("collapsed");
-  });
-});
-
